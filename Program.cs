c5ade3a0c0c4b0a87f0c507c58db9bafa8f6bd49
Bilal Sohail Mirza <bsohail420@gmail.com>
2024-12-29 23:57:31 +0500

    First Commit.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using api_tutorial.Repository;
+using finshark.Data;
+using finshark.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SampleConnection"));
+});
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
