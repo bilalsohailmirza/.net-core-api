a28000e1412a953d27b97628bef032e1ce91a0a2
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-01-11 02:45:04 +0500

    Changed Application DB Context to Identity DB Context.

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,11 +1,19 @@
 using finshark.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 // using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace finshark.Data
 {
-    public class ApplicationDBContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    public class ApplicationDBContext: IdentityDbContext<User>
     {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+        
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comments { get; set; }
     }
